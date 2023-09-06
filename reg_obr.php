9788064742062937c7c2107ac0d8926343886f03
unit203 <142182957+unit203@users.noreply.github.com>
2023-09-06 17:08:35 +0300

    Update reg_obr.php

--- reg_obr.php
+++ reg_obr.php
@@ -2,44 +2,25 @@
 header('Content-Type: text/html; charset=utf-8');
 
 $mysqli = mysqli_connect("localhost", "xcushycx_trein", "trein", "xcushycx_trein");
-if ($mysqli == false){
-    print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
-}else{
-    print("Соединение установлено успешно");
-    $name = $_POST["name"];
-    $lastname = $_POST["lastname"];
-    $email = $_POST["email"];
-    $pass = $_POST["pass"];
 
-    $result = $mysqli-> query("SELECT * FROM `table1` WHERE `email`");
+if ($mysqli == false) {
+  print("error");
+} else {
+//   print("Соединение установлено успешно");
+  $name = $_POST["name"];
+  $lastname = $_POST["lastname"];
+  $email = $_POST["email"];
+  $pass = $_POST["pass"];
 
-    // var_dump($result);
-    // var_dump($result->num_rows);
+  $result = $mysqli->query("SELECT * FROM `users` WHERE `email`='$email'");
 
-    if ($result->num_rows != 0){
-        print("Такой пользователь уже существует");
-    } else {
-        $mysqli-> query("INSERT INTO `table1`(`id`, `name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
-        print("Пользователь успешно зарегистрирован");
-    }
 
-    // echo "Имя: $name<br>
-    // Фамилия: $lastname <br>
-    // Email: $email <br>
-    // Пароль: $pass <hr>";
-    
+  if ($result->num_rows != 0) {
+    print("exist");
+  } else {
+    $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
+    print('true');
+  }
 }
-
-
 ?>
-
-
-
-
-
-
-
-
-
-
 
